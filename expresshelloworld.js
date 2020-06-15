0bfb7b5115c6517c0d8601c980ef5209a5dee143
movaismalik@yahoo.com <movaismalik@yahoo.com>
2020-06-15 02:30:22 -0700

    Added middleware

--- expresshelloworld.js
+++ expresshelloworld.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const serverIndex = require('serve-index');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Node World!'));
-app.listen(port, () => console.log('Example app listening at http://localhost:${port}'));
+app.get('/', (req, res) => {
+    res.send('Hello Node World!')
+});
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+});
+
+app.use('/nest', (req, res, next) => {
+    console.log('Request type: ', req.method);
+    next();
+});
+
+
+//Adding a middleware
+app.use('/apigateway', express.static('public'));
+app.use('/apigateway', serverIndex('public'));
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}')
+});
+
